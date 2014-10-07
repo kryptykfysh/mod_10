c761ab1ca5e201c1fd16ff0afc21a1bce9d090a4
Kryptyk Fysh <kryptykfysh@kryptykfysh.co.uk>
2014-10-08 10:46:22 +1100

    FInally working.

--- lib/mod_10.rb
+++ lib/mod_10.rb
@@ -17,12 +17,13 @@ module Mod10
 
   # Checks if a given value has a valid Mod10 check digit.
   def is_mod10?(value)
-    total = 0
-    value.to_s.reverse.split(//).map(&:to_i).each_with_index do |el, i|
-      el *= 2 if i.even?
-      el = el.to_s.split(//).reduce(0) { |sum, x| sum += x.to_i } if el > 9
-      total += el
-    end
-    total.to_s[-1] == '0'
+    digits = value.to_s.chars.map(&:to_i)
+    check_digit = digits.pop
+
+    sum = digits.reverse.each_slice(2).map do |x, y|
+      [(x * 2).divmod(10), y || 0]
+    end.flatten.inject(:+)
+
+    sum % 10 == check_digit
   end
 end
--- lib/mod_10/version.rb
+++ lib/mod_10/version.rb
@@ -1,3 +1,3 @@
 module Mod10
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
